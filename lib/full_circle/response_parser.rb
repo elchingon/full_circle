fb9996f87e266609b645e7c5f6f40471354108f8
Aaron Renner <aaron@animascodelabs.com>
2012-12-12 10:20:47 -0700

    Added todo note about future direction of response parser

--- lib/full_circle/response_parser.rb
+++ lib/full_circle/response_parser.rb
@@ -4,6 +4,10 @@ module FullCircle
     def self.parse(response)
       attrs = response.parsed_response
 
+      # TODO this could be much better. Maybe have individual parsers that are called
+      # based on a specific key like "events". This is a good gist for an idea of how
+      # to search deeply in nested hashes: https://gist.github.com/58257
+      
       if attrs["ad_getEventsResponse"]["events"].nil?
         []
       else
