2d22c0184066fcb4d955fd6737cf85295d63383f
Aaron Renner <aaron@animascodelabs.com>
2013-04-29 16:29:21 +0000

    Added a null cache implementation to allow for caching

--- lib/full_circle/connection.rb
+++ lib/full_circle/connection.rb
@@ -1,10 +1,15 @@
+require "net/http"
 module FullCircle
   class Connection
 
-    attr_reader :domain
+    attr_reader :domain, :cache
 
-    def initialize(domain)
+    # domain - domain of directory (ex. 360durango.com)
+    # args
+    #   - cache - Caching object to use
+    def initialize(domain, args={})
       @domain = domain
+      @cache = args.fetch(:cache){NullCache.new}
     end
 
     def base_uri
@@ -13,12 +18,28 @@ module FullCircle
 
     def call_api_method(method_name, query_params={})
       uri = URI(uri_string(method_name, query_params))
-      response_text = Net::HTTP.get(uri)
-      Response.new response_text
+
+      body = cache.fetch(uri) do
+        response_text = Net::HTTP.get(uri)
+        cache.store(uri, response_text)
+      end
+
+      Response.new body
+
     end
 
     private
     Response = Struct.new(:body)
+
+    NullCache = Class.new do
+      def fetch(key)
+        yield
+      end
+
+      def store(key, value)
+        value
+      end
+    end
 
     def uri_string(method_name, query_params)
       "#{base_uri}#{method_name}?#{query_params.to_query}"
--- spec/full_circle/connection_spec.rb
+++ spec/full_circle/connection_spec.rb
@@ -7,17 +7,42 @@ describe FullCircle::Connection do
   its(:domain) {should == "360durango.com"}
   its(:base_uri) {should == "http://api.360durango.com/1.0/"}
 
+  let!(:req_stub) {stub_http_request(:get, "api.360durango.com/1.0/ad.getCoupons").with(query: {adId: "81304"})
+    .to_return(body: "abc")
+  }
+
   describe ".call_api_method" do
 
     it "should request the correct uri" do
-      req_stub = stub_http_request(:get, "api.360durango.com/1.0/ad.getCoupons").with(query: {adId: "81304"})
-
       subject.call_api_method "ad.getCoupons", adId: "81304"
 
       req_stub.should have_been_requested
-
     end
 
+    it "should return the correct data" do
+      response = subject.call_api_method "ad.getCoupons", adId: "81304"
+      response.body.should eq("abc")
+    end
+
+    context "without cache" do
+      it "should make the http request each time" do
+        subject.call_api_method "ad.getCoupons", adId: "81304"
+        subject.call_api_method "ad.getCoupons", adId: "81304"
+
+        req_stub.should have_been_requested.twice
+      end
+    end
+
+    context "with cache" do
+      subject {FullCircle::Connection.new "360durango.com", cache: {}}
+
+      it "should make the http request each time" do
+        subject.call_api_method "ad.getCoupons", adId: "81304"
+        subject.call_api_method "ad.getCoupons", adId: "81304"
+
+        req_stub.should have_been_requested.once
+      end
+    end
 
   end
 
