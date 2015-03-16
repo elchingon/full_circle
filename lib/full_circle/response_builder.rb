f8f6e70363165dc3de83e716fc683f1620638c06
Aaron Renner <aaron@animascodelabs.com>
2015-03-16 14:34:39 -0600

    Extracting private method in ResponseBuilder for readability

--- lib/full_circle/response_builder.rb
+++ lib/full_circle/response_builder.rb
@@ -2,11 +2,17 @@ module FullCircle
   # @api private
   class ResponseBuilder
     def build(api_results, object_builder: ObjectBuilder.new)
-      objects = api_results.map do |result|
-        object_builder.from_api_hash(result)
-      end
+      objects = objects_from_api_hashes(api_results, object_builder)
 
       Response.new(objects)
+    end
+
+    private
+
+    def objects_from_api_hashes(api_results, object_builder)
+      api_results.map do |result|
+        object_builder.from_api_hash(result)
+      end
     end
   end
 end
