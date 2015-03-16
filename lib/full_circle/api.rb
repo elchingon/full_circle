99bdda237580b59c1fa83a9ee7087916445f82e3
Aaron Renner <aaron@animascodelabs.com>
2015-03-16 11:53:55 -0600

    Extracted private method in FullCircleAPI

--- lib/full_circle/api.rb
+++ lib/full_circle/api.rb
@@ -9,29 +9,30 @@ module FullCircle
 
     def fetch_events_for_ad(id)
       response = connection.call_api_method("ad.getEvents", adId: id)
-      pr = ParsedResponse.new(response.body)
-      builder = ResponseBuilder.new pr.results, metadata: pr.metadata
-      builder.build.results
+      process_response(response)
     end
 
     def fetch_coupons_for_ad(id)
       response = connection.call_api_method("ad.getCoupons", adId: id)
-      pr = ParsedResponse.new(response.body)
-      builder = ResponseBuilder.new pr.results, metadata: pr.metadata
-      builder.build.results
+      process_response(response)
     end
 
     def fetch_event_areas()
       response = connection.call_api_method("city.getEventAreas")
-      pr = ParsedResponse.new(response.body)
-      builder = ResponseBuilder.new pr.results, metadata: pr.metadata
-      builder.build.results
+      process_response(response)
     end
 
     def fetch_upcoming_events(params={})
       response = connection.call_api_method("city.getUpcomingEvents", params)
+      process_response(response, object_builder: UpcomingEventBuilder.new)
+    end
+
+    private
+
+    def process_response(response, object_builder: ObjectBuilder.new)
       pr = ParsedResponse.new(response.body)
-      builder = ResponseBuilder.new pr.results, metadata: pr.metadata, object_builder: UpcomingEventBuilder.new
+      builder = ResponseBuilder.new pr.results, metadata: pr.metadata,
+        object_builder: object_builder
       builder.build.results
     end
   end
