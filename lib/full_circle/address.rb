a49134071c6d497d2aaaaf4536d7546eaebeac59
Aaron Renner <aaron@animascodelabs.com>
2015-03-17 14:39:56 -0600

    Fixed zip -> zip_code on address model

--- lib/full_circle/address.rb
+++ lib/full_circle/address.rb
@@ -9,7 +9,7 @@ module FullCircle
       attribute :addr_1, String
       attribute :city, String
       attribute :state, String
-      attribute :zip, String
+      attribute :zip_code, String
       attribute :phone, String
       attribute :latitude, Float
       attribute :longitude, Float
