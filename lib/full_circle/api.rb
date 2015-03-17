a1a83c12ad174d8b2c37e633267ff515471a8c50
Aaron Renner <aaron@animascodelabs.com>
2015-03-17 08:51:11 -0600

    Updatd api to take page and resultsPerPage parameters
    * Slimed down the integration tests in API spec

--- lib/full_circle/api.rb
+++ lib/full_circle/api.rb
@@ -3,27 +3,32 @@ module FullCircle
 
     attr_reader :connection
 
-    def initialize(connection, response_parser: ResponseParser.new)
+    def initialize(connection, response_parser: ResponseParser.new, results_per_page: 20)
       @connection=connection
       @response_parser = response_parser
+      @results_per_page = results_per_page
     end
 
-    def fetch_events_for_ad(id)
-      response_xml = connection.call_api_method("ad.getEvents", adId: id)
+    def fetch_events_for_ad(id,params={})
+      request_params = {page: 1, resultsPerPage: results_per_page, adId: id}.merge(params)
+      response_xml = connection.call_api_method("ad.getEvents", request_params)
       response_parser.parse(response_xml).entities
     end
 
-    def fetch_coupons_for_ad(id)
-      response_xml = connection.call_api_method("ad.getCoupons", adId: id)
+    def fetch_coupons_for_ad(id,params={})
+      request_params = {page: 1, resultsPerPage: results_per_page, adId: id}.merge(params)
+      response_xml = connection.call_api_method("ad.getCoupons", request_params)
       response_parser.parse(response_xml).entities
     end
 
-    def fetch_event_areas()
-      response_xml = connection.call_api_method("city.getEventAreas")
+    def fetch_event_areas(params={})
+      request_params = {page: 1, resultsPerPage: results_per_page}.merge(params)
+      response_xml = connection.call_api_method("city.getEventAreas", request_params)
       response_parser.parse(response_xml).entities
     end
 
     def fetch_upcoming_events(params={})
+      request_params = {page: 1, resultsPerPage: results_per_page}.merge(params)
       response_xml = connection.call_api_method("city.getUpcomingEvents", params)
       response_parser.parse(response_xml, entity_builder: Builders::UpcomingEventBuilder)
         .entities
@@ -31,6 +36,6 @@ module FullCircle
 
     private
 
-    attr_reader :response_parser
+    attr_reader :response_parser, :results_per_page
   end
 end
--- spec/full_circle/api_spec.rb
+++ spec/full_circle/api_spec.rb
@@ -3,86 +3,45 @@ require 'vcr'
 
 describe FullCircle::API do
 
-  let!(:api){FullCircle::API.new(FullCircle::Connection.new("360durango.com"))}
+  let(:connection){FullCircle::Connection.new("360durango.com")}
+  let(:api){FullCircle::API.new(connection)}
 
   describe "#fetch_events_for_ad" do
-    skip "calls the appropriate method on call_api_method" do
-      mock_connection = double()
-      expect(mock_connection).to receive(:call_api_method).with("ad.getEvents",{adId: "1"}) do
-        class ResponseDouble
-          def body
-          end
-        end
-        ResponseDouble.new
-      end
 
-      described_class.new(mock_connection).fetch_events_for_ad("1")
-    end
-
-    describe "event object", :vcr => {cassette_name: "single_get_events_response"} do
-      subject {api.fetch_events_for_ad("81213").first}
-      it_behaves_like "an event"
-    end
-
-    context "with one event" do
-      it "returns an array of one event" do
-        VCR.use_cassette "single_get_events_response" do
-          results = api.fetch_events_for_ad "81213"
-          expect(results).to be_a Array
-          results
-        end
-      end
-
-    end
-
-    context "with multiple events" do
-      it "returns an array of multiple events" do
-        VCR.use_cassette "multple_get_events_response" do
-          results = api.fetch_events_for_ad "89690"
-          expect(results).to be_a Array
-          expect(results.length > 1).to eq(true)
-        end
+    it "returns an array of one event" do
+      VCR.use_cassette "get_events_for_ad_response" do
+        results = api.fetch_events_for_ad "81299"
+        expect(results.length > 0).to eq(true)
       end
     end
 
     context "with no events" do
       it "returns an empty array" do
-        VCR.use_cassette "empty_get_events_response" do
+        VCR.use_cassette "empty_get_events_for_ad_response" do
           results = api.fetch_events_for_ad "1"
           expect(results).to eq([])
         end
       end
     end
-
   end
 
 
   describe "#fetch_coupons_for_ad" do
 
-    skip "calls the appropriate method on call_api_method" do
-      mock_connection = double()
-      expect(mock_connection).to receive(:call_api_method).with("ad.getCoupons",{adId: "1"})
+    it "calls the appropriate method on call_api_method" do
+      parser = instance_double("FullCircle::Parsing::ResponseParser",
+        parse: double('result', entities: []))
+      mock_connection = instance_double("FullCircle::Connection")
+      allow(mock_connection).to receive(:call_api_method)
 
-      described_class.new(mock_connection).fetch_coupons_for_ad("1")
+      described_class.new(mock_connection, response_parser: parser).fetch_coupons_for_ad("1", page: 2)
+      expect(mock_connection).to have_received(:call_api_method).with("ad.getCoupons",{adId: "1", page: 2, resultsPerPage: 20})
     end
 
-    context "with one coupon" do
-      it "returns an array of one coupon" do
-        VCR.use_cassette "single_get_coupons_response" do
-          results = api.fetch_coupons_for_ad "123094"
-          expect(results).to be_an(Array)
-          expect(results.length).to eq(1)
-        end
-      end
-    end
-
-    context "with multiple coupons" do
-      it "returns an array of multiple coupons" do
-        VCR.use_cassette "multiple_get_coupons_response" do
-          results = api.fetch_coupons_for_ad "82196"
-          expect(results).to be_a(Array)
-          expect(results.length).to eq(3)
-        end
+    it "returns an array of coupons" do
+      VCR.use_cassette "get_coupons_for_ad_response" do
+        results = api.fetch_coupons_for_ad "89588"
+        expect(results.length > 0).to eq(true)
       end
     end
 
@@ -97,33 +56,17 @@ describe FullCircle::API do
   end
 
   describe "#fetch_event_areas" do
-    skip "calls the appropriate method on call_api_method" do
-      mock_connection = double()
-      expect(mock_connection).to receive(:call_api_method).with("city.getEventAreas")
 
-      described_class.new(mock_connection).fetch_event_areas
-    end
-
-    context "with one event area" do
-      it "returns an array of one event area" do
-        VCR.use_cassette "single_event_areas_response" do
-          results = api.fetch_event_areas
-          expect(results.length).to eq(1)
-        end
-      end
-    end
-
-    context "with multiple event areas" do
-      it "returns an array of multiple event areas" do
-        VCR.use_cassette "multiple_event_areas_response" do
-          results = api.fetch_event_areas
-          expect(results.length).to eq(7)
-        end
+    it "returns an array of event areas" do
+      VCR.use_cassette "event_areas_response" do
+        results = api.fetch_event_areas
+        expect(results.length > 0).to eq(true)
       end
     end
 
     context "with no event areas" do
-      let!(:api){FullCircle::API.new(FullCircle::Connection.new("1019thewave.com"))}
+      let(:connection){FullCircle::Connection.new("boatersbluepages.com")}
+
       it "returns an empty array" do
         VCR.use_cassette "empty_event_area_response" do
           results = api.fetch_event_areas
@@ -136,40 +79,15 @@ describe FullCircle::API do
 
   describe "#fetch_upcoming_events" do
 
-    skip "calls the appropriate method on call_api_method" do
-      mock_connection = double()
-      expect(mock_connection).to receive(:call_api_method).with("city.getUpcomingEvents",{})
-
-      described_class.new(mock_connection).fetch_upcoming_events
-    end
-
-    describe "event object", vcr: {cassette_name: "single_get_upcoming_events_response"} do
-
-      subject {api.fetch_upcoming_events(areaId:"592").first}
-      it_behaves_like "an event"
-    end
-
-    context "with one event" do
-      it "returns an array of one event" do
-        VCR.use_cassette "single_get_upcoming_events_response" do
-          results = api.fetch_upcoming_events areaId: "592"
-          expect(results.length).to eq(1)
-        end
-      end
-
-    end
-
-    context "with multiple events" do
-      it "returns an array of multiple events" do
-        VCR.use_cassette "multiple_get_upcoming_events_response" do
-          results = api.fetch_upcoming_events
-          expect(results.length > 1).to eq(true)
-        end
+    it "returns an array of multiple events" do
+      VCR.use_cassette "get_upcoming_events_response" do
+        results = api.fetch_upcoming_events
+        expect(results.length > 1).to eq(true)
       end
     end
 
     context "with no events" do
-      let!(:api){FullCircle::API.new(FullCircle::Connection.new("boatersbluepages.com"))}
+      let(:connection){FullCircle::Connection.new("boatersbluepages.com")}
       it "returns an empty array" do
         VCR.use_cassette "empty_get_upcoming_events_response" do
           results = api.fetch_upcoming_events
@@ -177,7 +95,5 @@ describe FullCircle::API do
         end
       end
     end
-
   end
-
 end
